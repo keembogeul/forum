d4347b05e4b3b9b65ba83287a002b4b7437993a7
keembogeul <318qhtmf@naver.com>
2023-05-18 18:07:27 +0900

    mongoDB 연결 후 게시글 리스트 화면 보여주기

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+  // DB 입출력 코드는 server component 안에서만 사용하도록 권장
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+
+  return <div>안녕</div>;
+}
